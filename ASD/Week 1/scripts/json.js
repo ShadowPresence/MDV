ce766624fa559f3390c08098eade8e3eaec63e6c
ShadowPresence <painsworth@fullsail.edu>
2012-11-02 20:12:28 -0400

    branch sync

--- ASD/Week 1/scripts/json.js
+++ ASD/Week 1/scripts/json.js
@@ -1,190 +1,181 @@
-/*
-		Title: Fruition (Gold)
-		Author: Philip Ainsworth
-		Class: Mobile Interfaces and Usability
-		Session: 1206
-		Week: 4
-*/
-
-var instaBusy = {
 	"item1": {
-		"taskName":["Item:","Bacon"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Shopping"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:",""],
-		"dueDate":["Due Date:",""],
-		"priority":["Priority:","0"]
+		"item":"Bacon",
+		"category":"Home",
+		"projects":"Shopping",
+		"notes":"",
+		"startDate":"",
+		"dueDate":"",
+		"priority":"0"
 	},
 	"item2": {
-		"taskName":["Item:","Eggs"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Shopping"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:",""],
-		"dueDate":["Due Date:",""],
-		"priority":["Priority:","0"]
+		"taskName":"Eggs",
+		"category":"Home",
+		"projects":"Shopping",
+		"notes":"",
+		"startDate":"",
+		"dueDate":"",
+		"priority":"0"
 	},
 	"item3": {
-		"taskName":["Item:","Bread"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Shopping"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:",""],
-		"dueDate":["Due Date:",""],
-		"priority":["Priority:","0"]
+		"taskName":"Bread",
+		"category":"Home",
+		"projects":"Shopping",
+		"notes":"",
+		"startDate":"",
+		"dueDate":"",
+		"priority":"0"
 	},
 	"item4": {
-		"taskName":["Item:","Milk"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Shopping"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:",""],
-		"dueDate":["Due Date:",""],
-		"priority":["Priority:","0"]
+		"taskName":"Milk",
+		"category":"Home",
+		"projects":"Shopping",
+		"notes":"",
+		"startDate":"",
+		"dueDate":"",
+		"priority":"0"
 	},
 	"item5": {
-		"taskName":["Item:","Cheese"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Shopping"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:",""],
-		"dueDate":["Due Date:",""],
-		"priority":["Priority:","0"]
+		"taskName":"Cheese",
+		"category":"Home",
+		"projects":"Shopping",
+		"notes":"",
+		"startDate":"",
+		"dueDate":"",
+		"priority":"0"
 	},
 	"item6": {
-		"taskName":["Item:","Project 2"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Homework"],
-		"notes":["Notes:","Split week 1 app into 2 apps, Gold & Bronze. Gold is ideal app bronze offers alternatives."],
-		"startDate":["Start Date:","2012-06-03T15:32"],
-		"dueDate":["Due Date:","2012-06-07T23:59"],
-		"priority":["Priority:","10"]
+		"taskName":"Project 2",
+		"category":"Home",
+		"projects":"Homework",
+		"notes":"Split week 1 app into 2 apps, Gold & Bronze. Gold is ideal app bronze offers alternatives.",
+		"startDate":"2012-06-03T15:32",
+		"dueDate":"2012-06-07T23:59",
+		"priority":"10"
 	},
 	"item7": {
-		"taskName":["Item:","Project 2 Video Review"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Homework"],
-		"notes":["Notes:","Review apps with decision justification"],
-		"startDate":["Start Date:","2012-06-06T09:00"],
-		"dueDate":["Due Date:","2012-06-07T23:59"],
-		"priority":["Priority:","9"]
+		"taskName":"Project 2 Video Review",
+		"category":"Home",
+		"projects":"Homework",
+		"notes":"Review apps with decision justification",
+		"startDate":"2012-06-06T09:00",
+		"dueDate":"2012-06-07T23:59",
+		"priority":"9"
 	},
 	"item8": {
-		"taskName":["Item:","Week 2 Discussion Video"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Homework"],
-		"notes":["Notes:","Discussion for week 2"],
-		"startDate":["Start Date:","2012-06-06T13:00"],
-		"dueDate":["Due Date:","2012-06-07T23:59"],
-		"priority":["Priority:","8"]
+		"taskName":"Week 2 Discussion Video",
+		"category":"Home",
+		"projects":"Homework",
+		"notes":"Discussion for week 2",
+		"startDate":"2012-06-06T13:00",
+		"dueDate":"2012-06-07T23:59",
+		"priority":"8"
 	},
 	"item9": {
-		"taskName":["Item:","Pick up kids"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Scheduled"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:","2012-06-04T08:00"],
-		"dueDate":["Due Date:","2012-06-04T15:15"],
-		"priority":["Priority:","0"]
+		"taskName":"Pick up kids",
+		"category":"Home",
+		"projects":"Scheduled",
+		"notes":"",
+		"startDate":"2012-06-04T08:00",
+		"dueDate":"2012-06-04T15:15",
+		"priority":"0"
 	},
 	"item10": {
-		"taskName":["Item:","Pick up kids"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Scheduled"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:","2012-06-05T08:00"],
-		"dueDate":["Due Date:","2012-06-05T15:15"],
-		"priority":["Priority:","0"]
+		"taskName":"Pick up kids",
+		"category":"Home",
+		"projects":"Scheduled",
+		"notes":"",
+		"startDate":"2012-06-05T08:00",
+		"dueDate":"2012-06-05T15:15",
+		"priority":"0"
 	},
 	"item11": {
-		"taskName":["Item:","Pick up kids"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Scheduled"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:","2012-06-06T08:00"],
-		"dueDate":["Due Date:","2012-06-06T15:15"],
-		"priority":["Priority:","0"]
+		"taskName":"Pick up kids",
+		"category":"Home",
+		"projects":"Scheduled",
+		"notes":"",
+		"startDate":"2012-06-06T08:00",
+		"dueDate":"2012-06-06T15:15",
+		"priority":"0"
 	},
 	"item12": {
-		"taskName":["Item:","Pick up kids"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Scheduled"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:","2012-06-07T08:00"],
-		"dueDate":["Due Date:","2012-06-07T15:15"],
-		"priority":["Priority:","0"]
+		"taskName":"Pick up kids",
+		"category":"Home",
+		"projects":"Scheduled",
+		"notes":"",
+		"startDate":"2012-06-07T08:00",
+		"dueDate":"2012-06-07T15:15",
+		"priority":"0"
 	},
 	"item13": {
-		"taskName":["Item:","Pick up kids"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Scheduled"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:","2012-06-08T08:00"],
-		"dueDate":["Due Date:","2012-06-08T15:15"],
-		"priority":["Priority:","0"]
+		"taskName":"Pick up kids",
+		"category":"Home",
+		"projects":"Scheduled",
+		"notes":"",
+		"startDate":"2012-06-08T08:00",
+		"dueDate":"2012-06-08T15:15",
+		"priority":"0"
 	},
 	"item14": {
-		"taskName":["Item:","Drumline Auditions"],
-		"category":["Category:","Work"],
-		"projects":["Project:","Scheduled"],
-		"notes":["Notes:","Seniors get priority."],
-		"startDate":["Start Date:","2012-06-08T08:00"],
-		"dueDate":["Due Date:","2012-06-01T14:00"],
-		"priority":["Priority:","0"]
+		"taskName":"Drumline Auditions",
+		"category":"Work",
+		"projects":"Scheduled",
+		"notes":"Seniors get priority.",
+		"startDate":"2012-06-08T08:00",
+		"dueDate":"2012-06-01T14:00",
+		"priority":"0"
 	},
 	"item15": {
-		"taskName":["Item:","Inventory"],
-		"category":["Category:","Work"],
-		"projects":["Project:","Scheduled"],
-		"notes":["Notes:","Take inventory of drumline instruments."],
-		"startDate":["Start Date:","2012-06-05T08:00"],
-		"dueDate":["Due Date:","2012-06-08T14:00"],
-		"priority":["Priority:","0"]
+		"taskName":"Inventory",
+		"category":"Work",
+		"projects":"Scheduled",
+		"notes":"Take inventory of drumline instruments.",
+		"startDate":"2012-06-05T08:00",
+		"dueDate":"2012-06-08T14:00",
+		"priority":"0"
 	},
 	"item16": {
-		"taskName":["Item:","Dentist"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Appointment"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:","2012-06-21T10:00"],
-		"dueDate":["Due Date:","2012-06-22T11:30"],
-		"priority":["Priority:","9"]
+		"taskName":"Dentist",
+		"category":"Home",
+		"projects":"Appointment",
+		"notes":"",
+		"startDate":"2012-06-21T10:00",
+		"dueDate":"2012-06-22T11:30",
+		"priority":"9"
 	},
 	"item17": {
-		"taskName":["Item:","Dr. Aung"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Appointment"],
-		"notes":["Notes:","Standard check up"],
-		"startDate":["Start Date:","2012-06-21T10:00"],
-		"dueDate":["Due Date:","2012-06-22T12:30"],
-		"priority":["Priority:","9"]
+		"taskName":"Dr. Aung",
+		"category":"Home",
+		"projects":"Appointment",
+		"notes":"Standard check up",
+		"startDate":"2012-06-21T10:00",
+		"dueDate":"2012-06-22T12:30",
+		"priority":"9"
 	},
 	"item18": {
-		"taskName":["Item:","Mow Lawn"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Household"],
-		"notes":["Notes:",""],
-		"startDate":["Start Date:","2012-06-09T08:00"],
-		"dueDate":["Due Date:","2012-06-11T17:30"],
-		"priority":["Priority:","5"]
+		"taskName":"Mow Lawn",
+		"category":"Home",
+		"projects":"Household",
+		"notes":"",
+		"startDate":"2012-06-09T08:00",
+		"dueDate":"2012-06-11T17:30",
+		"priority":"5"
 	},
 	"item19": {
-		"taskName":["Item:","Plant Flowers"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Household"],
-		"notes":["Notes:","5 bulbs need to be planted in various places"],
-		"startDate":["Start Date:","2012-06-09T08:00"],
-		"dueDate":["Due Date:","2012-06-11T17:30"],
-		"priority":["Priority:","7"]
+		"taskName":"Plant Flowers",
+		"category":"Home",
+		"projects":"Household",
+		"notes":"5 bulbs need to be planted in various places",
+		"startDate":"2012-06-09T08:00",
+		"dueDate":"2012-06-11T17:30",
+		"priority":"7"
 	},
 	"item20": {
-		"taskName":["Item:","Repair Wall"],
-		"category":["Category:","Home"],
-		"projects":["Project:","Household"],
-		"notes":["Notes:","Bathroom needs joint compound and paint"],
-		"startDate":["Start Date:","2012-06-09T08:00"],
-		"dueDate":["Due Date:","2012-06-11T17:30"],
-		"priority":["Priority:","3"]
+		"taskName":"Repair Wall",
+		"category":"Home",
+		"projects":"Household",
+		"notes":"Bathroom needs joint compound and paint",
+		"startDate":"2012-06-09T08:00",
+		"dueDate":"2012-06-11T17:30",
+		"priority":"3"
 	}
 }
