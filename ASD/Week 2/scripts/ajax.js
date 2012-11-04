111926f6d1e98c918f44d569c4d72b688496fbc8
ShadowPresence <painsworth@fullsail.edu>
2012-11-04 00:15:26 -0400

    Finally working as expected

--- ASD/Week 2/scripts/ajax.js
+++ ASD/Week 2/scripts/ajax.js
@@ -5,16 +5,16 @@ var it;
 var render = function (it){
 	var setDiv = $('<div>'),
 		setHead = $('<h3>');
+	setDiv.appendTo('#hanger');
 	setDiv.attr("data-role", "collapsible");
 	setDiv.attr("data-theme", "a");
-	setHead.html(it.item.toUpperCase());
-	setDiv.appendTo('#hanger');
 	setDiv.append(setHead);
+	setHead.html(it.item.toUpperCase());
 	for (var n in it) {
 		if (n !== "item") {
-			var setP = $('<p>');
-			setP.html(n.toUpperCase() + ": " + it[n]);
-			setDiv.append(setP);
+			setPara = $('<p>');
+			setDiv.append(setPara);
+			setPara.html(n.toUpperCase() + ": " + it[n]);
 		};
 	};
 };
@@ -29,8 +29,26 @@ $(function(){
 			for (var i = 0; i < instaBusy.Items.length; i++) {
 				it = instaBusy.Items[i];
 				render(it);
+				$("#hanger").trigger('create');
 				// $('#hanger').listview('refresh');
 			};
 		}
 	});
 });
+
+
+// var proWork = function (f, n, name) {
+// 	var proAr,
+// 		tarList = gid(name + n),
+// 		setDiv = document.createElement('div'),
+// 		listing = document.createElement('h3'),
+// 		para = document.createElement('p');
+// 	tarList.appendChild(setDiv);
+// 	setDiv.setAttribute("data-role", "collapsible");
+// 	setDiv.setAttribute("data-theme", "a");
+// 	setDiv.appendChild(listing);
+// 	listing.innerHTML = proAr[f];
+// 	setDiv.appendChild(para);
+// 	para.id = name + f + "" + n;
+// 	getJSON(proAr[f], f, n, name);
+// };
