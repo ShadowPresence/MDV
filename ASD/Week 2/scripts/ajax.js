0c2e1530decb99a7750579e38dbd85f5d866589d
ShadowPresence <painsworth@fullsail.edu>
2012-11-03 23:51:51 -0400

    fixed I think

--- ASD/Week 2/scripts/ajax.js
+++ ASD/Week 2/scripts/ajax.js
@@ -17,6 +17,7 @@ var render = function (it){
 			setDiv.append(setP);
 		};
 	};
+};
 
 $(function(){
 	$('#hanger').empty();
@@ -33,45 +34,3 @@ $(function(){
 		}
 	});
 });
-
-// //
-// var it,
-// 	setDiv;
-
-// var render = function (style, it){
-// 	if (style=="heading") {
-// 		var setDiv = $('<div>'),
-// 			setHead = $('<h3>');
-// 		setDiv.attr("data-role", "collapsible");
-// 		setDiv.attr("data-theme", "a");
-// 		setHead.html(it.item.toUpperCase());
-// 		setDiv.appendTo('#set');
-// 		setDiv.append(setHead);
-// 	} else {
-// 		for (var n in it) {
-// 			if (n !== "item") {
-// 				var setP = $('<p>');
-// 				setP.html(n.toUpperCase() + ": " + it[n]);
-// 				setDiv.append(setP);
-// 			};
-// 		};
-// 		// $('#hanger').listview('refresh');
-// 	};
-// };
-
-// $(function(){
-// 	$('#hanger').empty();
-// 	$.ajax({
-// 		url: "xhr/data.json",
-// 		type: "GET",
-// 		dataType: "json",
-// 		success: function(instaBusy){
-// 			for (var i = 0; i < instaBusy.Items.length; i++) {
-// 				it = instaBusy.Items[i];
-// 				render("heading", it);
-// 				render("desc", it);
-// 			};
-// 		}
-// 	});
-// });
-
