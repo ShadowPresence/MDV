3511db7b1b9622ca37929f58770b331a1861d1c4
ShadowPresence <painsworth@fullsail.edu>
2012-11-04 09:04:16 -0500

    Branch sync

--- ASD/Week 2/scripts/ajax.js
+++ ASD/Week 2/scripts/ajax.js
@@ -1,111 +1,64 @@
-//
-<<<<<<< HEAD
-var it;
+/*
+		Title: Fruition (Gold)
+		Author: Philip Ainsworth
+		Class: Advanced Scalable Data Infrastructure
+		Session: 1211
+		Week: 2
+*/
 
-var render = function (it){
-	var setDiv = $('<div>'),
-		setHead = $('<h3>');
-	setDiv.attr("data-role", "collapsible");
-	setDiv.attr("data-theme", "a");
-	setHead.html(it.item.toUpperCase());
-	setDiv.appendTo('#hanger');
-	setDiv.append(setHead);
-	for (var n in it) {
-		if (n !== "item") {
-			var setP = $('<p>');
-			setP.html(n.toUpperCase() + ": " + it[n]);
-			setDiv.append(setP);
-		};
-	};
-=======
-var it
+// This function adds empty string testing to jquery found at: http://laktek.com/2011/01/07/jquery-isblank/
+// Could also use if(it[n] === "") but the isBlank method tests for more than just an empty string, it also tests for null, white space, "0", or undefined
+(function($){
+  $.isBlank = function(obj){
+    return(!obj || $.trim(obj) === "");
+  };
+})(jQuery);
 
-var render = function (style, it){
-	if (style=="heading") {
-		var setDiv = $('<li>');
-		setDiv.id = Math.floor(Math.random()*1000000);
-		setDiv.html(it.item.toUpperCase());
+var it,
+	render = function (it){
+		var setDiv = $('<div>'),
+			setHead = $('<h3>'),
+			setView = $('<ul>'),
+			setList = $('<li>');
 		setDiv.appendTo('#hanger');
-	} else {
+		setDiv.attr("data-role", "collapsible");
+		setDiv.attr("data-theme", "a");
+		setDiv.append(setHead, setView);
+		setHead.html(it.item.toUpperCase());
+		setView.attr("data-role", "listview");
+		setView.append(setList);
 		for (var n in it) {
 			if (n !== "item") {
-				var setDiv2 = $('<div>');
-				setDiv2.attr("data-role", "collapsible-set");
-				setDiv2.attr("data-theme", "a");
-				setDiv2.html(n.toUpperCase() + ": " + it[n]);
-				setDiv2.appendTo('setDiv');
+				if ($.isBlank(it[n])) {
+					console.log("The " + n + " field in the " + it.item + " entry, does not contain any data, and will not be assimilated.")
+				} else {
+				setSub = $('<h3>');
+				setDesc = $('<p>');
+				setList.append(setSub, setDesc);
+				setSub.html(n.toUpperCase());
+				setDesc.html(it[n]);
+				};
 			};
 		};
 	};
-	
-	$('#hanger').listview('refresh');
->>>>>>> origin/master
-};
 
+// This function processes the json data file. 
 $(function(){
 	$('#hanger').empty();
 	$.ajax({
 		url: "xhr/data.json",
 		type: "GET",
 		dataType: "json",
-		success: function(instaBusy){
-			for (var i = 0; i < instaBusy.Items.length; i++) {
-				it = instaBusy.Items[i];
-<<<<<<< HEAD
+		success: function(x){
+			//This loops through each object(item) in the json.data file.
+			for (var i = 0; i < x.Items.length; i++) {
+				it = x.Items[i];
+				console.log(it);
+				// render() loops through each individual item 
 				render(it);
-				// $('#hanger').listview('refresh');
-=======
-				render("heading", it);
-				render("desc", it);
->>>>>>> origin/master
+				// trigger.('create') tells the browser to render the dynamic code using JQM styling
+				$("#hanger").trigger('create');
 			};
 		}
 	});
 });
-
-<<<<<<< HEAD
-
-// //
-// var it,
-// 	setDiv;
-
-// var render = function (style, it){
-// 	if (style=="heading") {
-// 		var setDiv = $('<div>'),
-// 			setHead = $('<h3>');
-// 		setDiv.attr("data-role", "collapsible");
-// 		setDiv.attr("data-theme", "a");
-// 		setHead.html(it.item.toUpperCase());
-// 		setDiv.appendTo('#set');
-// 		setDiv.append(setHead);
-// 	} else {
-// 		for (var n in it) {
-// 			if (n !== "item") {
-// 				var setP = $('<p>');
-// 				setP.html(n.toUpperCase() + ": " + it[n]);
-// 				setDiv.append(setP);
-// 			};
-// 		};
-// 		// $('#hanger').listview('refresh');
-// 	};
-// };
-
-// $(function(){
-// 	$('#hanger').empty();
-// 	$.ajax({
-// 		url: "xhr/data.json",
-// 		type: "GET",
-// 		dataType: "json",
-// 		success: function(instaBusy){
-// 			for (var i = 0; i < instaBusy.Items.length; i++) {
-// 				it = instaBusy.Items[i];
-// 				render("heading", it);
-// 				render("desc", it);
-// 			};
-// 		}
-// 	});
-// });
-
-=======
->>>>>>> origin/master
- 
