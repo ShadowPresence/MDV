855231efb06581da9d588a70d84bf4885397d8fb
ShadowPresence <painsworth@fullsail.edu>
2012-11-03 23:25:55 -0400

    wtf

--- ASD/Week 2/scripts/ajax.js
+++ ASD/Week 2/scripts/ajax.js
@@ -1,4 +1,26 @@
 //
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+var it;
+
+var render = function (it){
+	var setDiv = $('<div>'),
+		setHead = $('<h3>');
+	setDiv.attr("data-role", "collapsible");
+	setDiv.attr("data-theme", "a");
+	setHead.html(it.item.toUpperCase());
+	setDiv.appendTo('#hanger');
+	setDiv.append(setHead);
+	for (var n in it) {
+		if (n !== "item") {
+			var setP = $('<p>');
+			setP.html(n.toUpperCase() + ": " + it[n]);
+			setDiv.append(setP);
+		};
+	};
+=======
+>>>>>>> origin/master
 var it
 
 var render = function (style, it){
@@ -20,6 +42,10 @@ var render = function (style, it){
 	};
 	
 	$('#hanger').listview('refresh');
+<<<<<<< HEAD
+=======
+>>>>>>> origin/master
+>>>>>>> origin/master
 };
 
 $(function(){
@@ -31,11 +57,69 @@ $(function(){
 		success: function(instaBusy){
 			for (var i = 0; i < instaBusy.Items.length; i++) {
 				it = instaBusy.Items[i];
+<<<<<<< HEAD
 				render("heading", it);
 				render("desc", it);
+=======
+<<<<<<< HEAD
+				render(it);
+				// $('#hanger').listview('refresh');
+=======
+				render("heading", it);
+				render("desc", it);
+>>>>>>> origin/master
+>>>>>>> origin/master
 			};
 		}
 	});
 });
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+
+// //
+// var it,
+// 	setDiv;
+
+// var render = function (style, it){
+// 	if (style=="heading") {
+// 		var setDiv = $('<div>'),
+// 			setHead = $('<h3>');
+// 		setDiv.attr("data-role", "collapsible");
+// 		setDiv.attr("data-theme", "a");
+// 		setHead.html(it.item.toUpperCase());
+// 		setDiv.appendTo('#set');
+// 		setDiv.append(setHead);
+// 	} else {
+// 		for (var n in it) {
+// 			if (n !== "item") {
+// 				var setP = $('<p>');
+// 				setP.html(n.toUpperCase() + ": " + it[n]);
+// 				setDiv.append(setP);
+// 			};
+// 		};
+// 		// $('#hanger').listview('refresh');
+// 	};
+// };
+
+// $(function(){
+// 	$('#hanger').empty();
+// 	$.ajax({
+// 		url: "xhr/data.json",
+// 		type: "GET",
+// 		dataType: "json",
+// 		success: function(instaBusy){
+// 			for (var i = 0; i < instaBusy.Items.length; i++) {
+// 				it = instaBusy.Items[i];
+// 				render("heading", it);
+// 				render("desc", it);
+// 			};
+// 		}
+// 	});
+// });
+
+=======
+>>>>>>> origin/master
+>>>>>>> origin/master
  
