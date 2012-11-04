ff3a66340e840a1665a9f13b681e2ae104a47268
ShadowPresence <painsworth@fullsail.edu>
2012-11-04 08:00:41 -0500

    'Tis working perfectly...

--- ASD/Week 2/scripts/ajax.js
+++ ASD/Week 2/scripts/ajax.js
@@ -1,54 +1,64 @@
-//
+/*
+		Title: Fruition (Gold)
+		Author: Philip Ainsworth
+		Class: Advanced Scalable Data Infrastructure
+		Session: 1211
+		Week: 2
+*/
 
-var it;
+// This function adds empty string testing to jquery found at: http://laktek.com/2011/01/07/jquery-isblank/
+// Could also use if(it[n] === "") but the isBlank method tests for more than just an empty string, it also tests for null, white space, "0", or undefined
+(function($){
+  $.isBlank = function(obj){
+    return(!obj || $.trim(obj) === "");
+  };
+})(jQuery);
 
-var render = function (it){
-	var setDiv = $('<div>'),
-		setHead = $('<h3>');
-	setDiv.appendTo('#hanger');
-	setDiv.attr("data-role", "collapsible");
-	setDiv.attr("data-theme", "a");
-	setDiv.append(setHead);
-	setHead.html(it.item.toUpperCase());
-	for (var n in it) {
-		if (n !== "item") {
-			setPara = $('<p>');
-			setDiv.append(setPara);
-			setPara.html(n.toUpperCase() + ": " + it[n]);
+var it,
+	render = function (it){
+		var setDiv = $('<div>'),
+			setHead = $('<h3>'),
+			setView = $('<ul>'),
+			setList = $('<li>');
+		setDiv.appendTo('#hanger');
+		setDiv.attr("data-role", "collapsible");
+		setDiv.attr("data-theme", "a");
+		setDiv.append(setHead, setView);
+		setHead.html(it.item.toUpperCase());
+		setView.attr("data-role", "listview");
+		setView.append(setList);
+		for (var n in it) {
+			if (n !== "item") {
+				if ($.isBlank(it[n])) {
+					console.log("The " + n + " field in the " + it.item + " entry, does not contain any data, and will not be assimilated.")
+				} else {
+				setSub = $('<h3>');
+				setDesc = $('<p>');
+				setList.append(setSub, setDesc);
+				setSub.html(n.toUpperCase());
+				setDesc.html(it[n]);
+				};
+			};
 		};
 	};
-};
 
+// This function processes the json data file. 
 $(function(){
 	$('#hanger').empty();
 	$.ajax({
 		url: "xhr/data.json",
 		type: "GET",
 		dataType: "json",
-		success: function(instaBusy){
-			for (var i = 0; i < instaBusy.Items.length; i++) {
-				it = instaBusy.Items[i];
+		success: function(x){
+			//This loops through each object(item) in the json.data file.
+			for (var i = 0; i < x.Items.length; i++) {
+				it = x.Items[i];
+				console.log(it);
+				// render() loops through each individual item 
 				render(it);
+				// trigger.('create') tells the browser to render the dynamic code using JQM styling
 				$("#hanger").trigger('create');
-				// $('#hanger').listview('refresh');
 			};
 		}
 	});
 });
-
-
-// var proWork = function (f, n, name) {
-// 	var proAr,
-// 		tarList = gid(name + n),
-// 		setDiv = document.createElement('div'),
-// 		listing = document.createElement('h3'),
-// 		para = document.createElement('p');
-// 	tarList.appendChild(setDiv);
-// 	setDiv.setAttribute("data-role", "collapsible");
-// 	setDiv.setAttribute("data-theme", "a");
-// 	setDiv.appendChild(listing);
-// 	listing.innerHTML = proAr[f];
-// 	setDiv.appendChild(para);
-// 	para.id = name + f + "" + n;
-// 	getJSON(proAr[f], f, n, name);
-// };
