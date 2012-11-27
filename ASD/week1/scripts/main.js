f45f6e15d35017070d3bb0c0a02225e31cbb4c77
ShadowPresence <painsworth@fullsail.edu>
2012-11-26 23:48:47 -0500

    sync

--- ASD/week1/scripts/main.js
+++ ASD/week1/scripts/main.js
@@ -6,7 +6,34 @@
 		Week: 1
 */
 
-$(document).bind('pageinit', function() {
+// This function adds empty string testing to jquery found at: http://laktek.com/2011/01/07/jquery-isblank/
+// Could also use if(it[n] === "") but the isBlank method tests for more than just an empty string, it also tests for null, white space, "0", or undefined
+(function($){
+  $.isBlank = function(obj){
+    return(!obj || $.trim(obj) === "");
+  };
+})(jQuery);
+
+	var it;
+
+// $('#list').on('pageinit', function(it){
+var list = function(it) {
+	var setAnchor = $('<a>'),
+		setText = $('<p>'),
+		setList = $('<li>'),
+		setDate = $('<p>');
+	setList.appendTo('#hanger'); // Adds <li></li>
+	setList.append(setAnchor); // <li><a></a></li>
+// need to get href to be dynamic
+	setAnchor.attr("href", "#detail"); // <li><a href="detail.html"></a></li>
+	setAnchor.append(setText); // <li><a href="detail.html"><p></p></a></li>
+	setAnchor.append(setDate); // <li><a href="detail.html"><p></p><p></p></a></li>
+	setText.append($('<strong>').html(it.company), " - " + it.account); // <li><a href="detail.html"><p><strong>Rivergreen</strong> - 7051234</p><p></p></a></li>
+	setDate.append($('<strong>').html("Date: "), it.date); // <li><a href="detail.html"><p><strong>Rivergreen</strong> - 7051234</p><p><strong>Date:</strong>2012-06-07</p></a></li>
+	$('#hanger').listview('refresh');
+};
+
+$('#list').on('pageinit', function() {
 	$('#hanger').empty();
 	$.getJSON('xhr/data.json', function(x){
 		//This loops through each object(item) in the json.data file.
@@ -17,7 +44,70 @@ $(document).bind('pageinit', function() {
 			list(it);
 			// trigger.('create') tells the browser to render the dynamic DIV element using JQM styling
 			// listview('refresh') renders a listview where trigger does not
-			$('#hanger').listview('refresh');
 		};
 	});
 });
+
+$('#delete').on('click', function(){
+	console.log("The delete button was pushed");
+});
+
+$('#edit').on('click', function(){
+	console.log("The edit button was pushed");
+});
+
+$('#save').on('click', function(){
+	console.log("The form submit button was pushed");
+});
+
+$('#detail').on('pageinit', function(){
+	console.log("The detail button was pushed");
+	$('#compHead').empty();
+	$.getJSON('xhr/data.json', function(y){
+		//This loops through each object(item) in the json.data file.
+		for (var i = 0; i < y.inspections.length; i++) {
+			that = y.inspections[i];
+			console.log(that);
+		};
+		var xyz = $('<hr />');
+		$('#compHead').append($('<p>').append($('<strong>').html("Date: "), that.date));
+		$('#compHead').append($('<h3>').html(that.company));
+		$('#compHead').append($('<p>').append($('<strong>').html("Account #:"), that.account));
+		xyz.appendTo($('#compHead'));
+		// $('#compHead').listview('refresh');
+	});
+});
+
+
+	// <p class="ui-li-aside"><strong>Date: </strong>11/24/12</p>
+	// 					<h1>Company 1</h1>
+	// 					<p><strong>Account #: </strong>7051234</p>
+	// 					<hr>
+
+		// For use when in detail view to pull info from all the fields
+		// for (var n in it) {
+		// 	if (n !== "company") {
+		// 		if ($.isBlank(it[n])) {
+		// 			console.log("The " + n + " field in the " + it.company + " entry, does not contain any data, and will not be assimilated.")
+		// 		} else {
+					
+		// 		};
+		// 	};
+		// };
+
+// This function processes the json data file. 
+// $(function(){
+// 	$('#hanger').empty();
+// 	$.getJSON('xhr/data.json', function(x){
+// 		//This loops through each object(item) in the json.data file.
+// 		for (var i = 0; i < x.inspections.length; i++) {
+// 			it = x.inspections[i];
+// 			console.log(it);
+// 			// render() loops through each individual item
+// 			list(it);
+// 			// trigger.('create') tells the browser to render the dynamic DIV element using JQM styling
+// 			// listview('refresh') renders a listview where trigger does not
+// 			$('#hanger').listview('refresh');
+// 		};
+// 	});
+// });
